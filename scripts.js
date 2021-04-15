9964a9d93369e3bcef28eeb01c6bbd30ce80b7a5
Felipe Pereira Pinto <ofelipepereira@gmail.com>
2021-04-15 19:18:49 -0300

    product cards populated + prices formated + image properly contained

--- scripts.js
+++ scripts.js
@@ -12,57 +12,42 @@ const API = {
   }
 }
 
-
-/* 
-<div id="products-grid">
-  <div class="product" id="prod-1">
-    <img src="assets/productPlaceholder.png" alt="Imagem do produto">
-    <h3>Nome do produto</h3>
-    <p class="description">Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata.</p>
-    <p class="oldPrice">De: R$23,99</p>
-    <p class="price">Por: R$19,99</p>
-    <p class="installments">ou 2x de R$9,99</p>
-
-    <button>Comprar</button>
-  </div>
-*/
-
 const DOM = {
   productsContainer: document.querySelector('#products-grid'),
-  product: document.querySelectorAll('.product'),
+  products: {},
   
   clearProducts() {
     DOM.productsContainer.innerHTML = ''
   },
 
   populateProducts() {
-    console.log(DOM.product[0])
-
-
-    API.products.forEach( product => {
-
-
-      
-      // console.log(product)
-      // console.log(product.image)
-      // console.log(product.name)
-      // console.log(product.oldPrice)
-      // console.log(product.price)
-      // console.log(product.description)
-      // console.log(product.installments.count)
-      // console.log(product.installments.value)
+    API.products.forEach( product => { 
+      this.productsContainer.innerHTML += `
+      <div class="product">
+        <img src="https:${product.image}" alt="Imagem do produto ${product.name}">
+        <h3>${product.name}</h3>
+        <p class="description">${product.description}</p>
+        <p class="oldPrice">De: ${Utils.parsePrice(product.oldPrice)}</p>
+        <p class="price">Por: ${Utils.parsePrice(product.price)}</p>
+        <p class="installments">ou ${product.installments.count}x de ${Utils.parsePrice(product.installments.value)}</p>
+        <button>Comprar</button>
+      </div>`
     })
+  }
+}
+
+const Utils = {
+  parsePrice(price) {
+    return 'R$' + price.toFixed(2).replace('.',',')
   }
 }
 
 const App = {
   init() {
-    API.fetchProducts(API.url).then( () => {
-      DOM.populateProducts()
-
-    })
-
+    API.fetchProducts(API.url)
+      .then( () => DOM.populateProducts() )
   }
 }
 
 App.init()
+
